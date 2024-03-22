ab33f33bc2b2778d9698d6cfbd88852f53b6ae0f
Divya-saini15 <divya15sainiofficial@gmail.com>
2024-03-22 22:35:17 +0530

    section1 completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './hero-section/hero.jsx'; // Import Hero component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>
-          Divya saini
-        </h1>
-        <a
-          className="App-link"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Goku
-        </a>
-      </header>
+          <Hero/>     
     </div>
   );
 }
---
+++ src/hero-section/hero.jsx
@@ -0,0 +1,36 @@
+// src/Hero.js
+import React from 'react';
+import '../hero-section/hero.css'; // Import CSS file for styling
+import '../App.css';
+
+
+const Hero = () => {
+  return (
+  <>
+  <section>
+    <div className="main-div">
+      <div id="left-hero">
+        <p  id='p1' className='text-25'>DIGITAL BANKING</p>
+        <h1 className="text-65">Smart Financial Services for Everyone</h1>
+        <p className='text-15' id='p2'>Lorem ipsum dolor sit amet, consectetur adipiscing 
+        elit. Ut elit tellus, luctus nec ullamcorper mattis.
+        </p>
+        <button>Get started </button>
+      </div>
+      <div id="right-hero">
+
+  <img  id="card1" className='card image-popup ' src="https://askproject.net/deroly/wp-content/uploads/sites/119/2022/12/cc1-1.png" alt=""/>
+  
+  <img id="card2" className='card image-popup ' src="https://askproject.net/deroly/wp-content/uploads/sites/119/2022/12/cc3-1.png" alt=""/>
+
+  <img id="card3" className='card image-popup ' src="https://askproject.net/deroly/wp-content/uploads/sites/119/2022/12/cc2-1.png" alt=""/>
+
+  </div>
+  </div>
+  </section>
+
+  </>
+  );
+  };
+
+export default Hero;
